4fbb5145c0e4e46b46a2c194652d6d5e406fdff9
antoniogisondi <anto.gisondi@tiscali.it>
2023-05-29 22:09:54 +0200

    Exercise done

--- js/main.js
+++ js/main.js
@@ -7,6 +7,64 @@ document.body.appendChild(container);
 // CREO UNA TABELLA NEL CONTENITORE PER LA GRIGLIA
 let table = document.createElement("table")
 container.appendChild(table)
+
+ // CREO LA FUNZIONE CHE GENERA UN NUMERO CASUALE
+ function randomNumber(min, max){
+    return Math.floor(Math.random()*(max - min + 1) + min)
+}
+
+ // CREO LA FUNZIONE CHE MI POPOLA L'ARRAY DELLE BOMBE
+ function arrayBombPush(array_bomb, max){
+    let check = false;
+    let random_number;
+    while(check === false){
+        random_number = randomNumber(1, max);
+        if(!array_bomb.includes(random_number)){
+            check = true;
+        }
+    }
+    return random_number;
+}
+
+// CREO LA FUNZIONE CHE GENERA LA GRIGLIA NEL CONTENITORE
+function grid(caselle){
+    let message = document.getElementById('score')
+    const arrayBomb = []
+    table.innerHTML = ''
+    message.innerText = ''
+    let cnt = 1;
+    let game_over = false;
+    let score = 0;
+    for(let i = 0; i<caselle; i++){
+        let row = document.createElement("tr")
+        table.appendChild(row)
+        for(let j = 0; cnt<=10, j<caselle; j++){
+            let column = document.createElement("td")
+            row.appendChild(column)
+            let box = document.createElement("div")
+            column.appendChild(box)
+            let numberGrid = document.createTextNode(`${cnt}`)
+            box.appendChild(numberGrid)
+            box.classList.add('column')
+            box.addEventListener('click', function(){
+                  if(!arrayBomb.includes(parseInt(this.innerText))){
+                      this.classList.toggle("bg-color")
+                      score++
+                  }
+                  else{
+                      this.classList.add("bomb")
+                      game_over = true
+                      document.getElementById('score').innerText = `Hai perso! Il tuo punteggio è di: ${score}`
+                  }  
+            })
+            cnt++  
+        }
+    }
+    for(let y = 0; y < 16; y++){
+        let number = arrayBombPush(arrayBomb, cnt)
+        arrayBomb.push(number)
+    }
+}
     
 // CREO L'EVENTO CHE MI GENERA LA GRIGLIA IN BASE AL LIVELLO DI DIFFICOLTA' 
 let button = document.querySelector(".play")
@@ -18,7 +76,7 @@ button.addEventListener('click', function(){
    else if(select == 2){
     grid(9)
    }
-   else{
+   else if(select == 1){
     grid(10)
    }
 })
@@ -29,31 +87,6 @@ reset.addEventListener('click', function(){
   table.innerHTML = ''
 })
 
-// CREO LA FUNZIONE CHE GENERA LA GRIGLIA NEL CONTENITORE
-    function grid(caselle){
-        table.innerHTML = ''
-        let cnt = 1;
-        for(let i = 0; i<caselle; i++){
-            let row = document.createElement("tr")
-            table.appendChild(row)
-            for(let j = 0; cnt<=10, j<caselle; j++){
-                let column = document.createElement("td")
-                row.appendChild(column)
-                let box = document.createElement("div")
-                column.appendChild(box)
-                let numberGrid = document.createTextNode(`${cnt}`)
-                box.appendChild(numberGrid)
-                box.classList.add('column')
-                box.style.width = `calc(100% / ${numberGrid})`
-                box.style.height = box.style.width
-                box.style.padding = `35px 0`
-                box.addEventListener('click', function(){
-                    this.classList.toggle("bg-color")
-                    console.log(this.innerText)
-                })
-                cnt++  
-            }
-        }
-    }
 
-    grid(10)
+
+   
