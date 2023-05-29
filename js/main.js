ecd1d4ccadcfdf521de6b8179b91ca86cc87f3d5
antoniogisondi <anto.gisondi@tiscali.it>
2023-05-29 14:14:13 +0200

    First commit, copiato contenuto cartella dell'esercizio precedente e si prosegue con il nuovo

---
+++ js/main.js
@@ -0,0 +1,59 @@
+"use strict"
+// CREO UN CONTENITORE PER LA GRIGLIA NELL'HTML
+let container = document.createElement("div")
+container.setAttribute('class', 'container mt-5')
+document.body.appendChild(container);
+
+// CREO UNA TABELLA NEL CONTENITORE PER LA GRIGLIA
+let table = document.createElement("table")
+container.appendChild(table)
+    
+// CREO L'EVENTO CHE MI GENERA LA GRIGLIA IN BASE AL LIVELLO DI DIFFICOLTA' 
+let button = document.querySelector(".play")
+button.addEventListener('click', function(){
+   let select = document.querySelector(".form-select").value
+   if(select == 3){
+    grid(7)
+   }
+   else if(select == 2){
+    grid(9)
+   }
+   else{
+    grid(10)
+   }
+})
+
+// CREO L'EVENTO CHE RESETTA LA GRIGLIA 
+let reset = document.querySelector(".reset")
+reset.addEventListener('click', function(){
+  table.innerHTML = ''
+})
+
+// CREO LA FUNZIONE CHE GENERA LA GRIGLIA NEL CONTENITORE
+    function grid(caselle){
+        table.innerHTML = ''
+        let cnt = 1;
+        for(let i = 0; i<caselle; i++){
+            let row = document.createElement("tr")
+            table.appendChild(row)
+            for(let j = 0; cnt<=10, j<caselle; j++){
+                let column = document.createElement("td")
+                row.appendChild(column)
+                let box = document.createElement("div")
+                column.appendChild(box)
+                let numberGrid = document.createTextNode(`${cnt}`)
+                box.appendChild(numberGrid)
+                box.classList.add('column')
+                box.style.width = `calc(100% / ${numberGrid})`
+                box.style.height = box.style.width
+                box.style.padding = `35px 0`
+                box.addEventListener('click', function(){
+                    this.classList.toggle("bg-color")
+                    console.log(this.innerText)
+                })
+                cnt++  
+            }
+        }
+    }
+
+    grid(10)
---
+++ readme.md
@@ -0,0 +1,17 @@
+PROBLEMA: L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100. Ci saranno quindi 10 caselle per ognuna delle 10 righe. Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+SOLUZIONE:
+1 Creare la funzione che genera la griglia di gioco;
+2 Collegare la griglia di gioco ad un evento che la creerà in base al livello di difficoltà scelto;
+3 Al click del tasto play questa griglia viene creata al giocatore a schermo 
+4 Creare un evento che faccia cambiare colore alle caselle al click
+
+
+
+<!-- NUOVO PROBLEMA -->
+PROBLEMA: Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle. La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
